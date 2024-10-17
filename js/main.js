7e9bd87bab987d695f9093f9a182e2536c6a8f84
dariomicelii <dariomiceli01@gmail.com>
2024-10-17 16:14:53 +0200

    es2

--- js/main.js
+++ js/main.js
@@ -15,28 +15,40 @@
 //controlla che sia nella lista di chi può accedere,
 //stampa un messaggio appropriato sull'esito del controllo.
 
-let mailWithAccess = ["ciao34@gmail.com", "me@gmail.com", "agamennone45@alice.it", "menelao.paride@gmail.com"]
+// let mailWithAccess = ["ciao34@gmail.com", "me@gmail.com", "agamennone45@alice.it", "menelao.paride@gmail.com"]
 
-//Metodo base
-userMail = prompt("inserisci la tua mail:")
-for(let i = 0; i < mailWithAccess.length; i++){
-    if(mailWithAccess[i] === userMail){
-        console.log("Benvenuto!")
-    }else{
-        console.log("Oops, non sembri essere loggato =(");
+// //Metodo base
+// userMail = prompt("inserisci la tua mail:")
+// for(let i = 0; i < mailWithAccess.length; i++){
+//     if(mailWithAccess[i] === userMail){
+//         console.log("Benvenuto!")
+//     }else{
+//         console.log("Oops, non sembri essere loggato =(");
         
-    }
-}
+//     }
+// }
 
-//Operatore ternario
-userMail = prompt("inserisci la tua mail:")
-for(let i = 0; i < mailWithAccess.length; i++){
-    console.log (mailWithAccess[i] === userMail ? "Benvenuto!" : "Oops, non sembri essere loggato! =(")
-}
-
+// //Operatore ternario
+// userMail = prompt("inserisci la tua mail:")
+// for(let i = 0; i < mailWithAccess.length; i++){
+//     console.log (mailWithAccess[i] === userMail ? "Benvenuto!" : "Oops, non sembri essere loggato! =(")
+// }
 
 
 
 //GIOCO DEI DADI
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const userGame = parseInt(Math.random() * 7); 
+const computerGame = parseInt(Math.random() * 7);
+console.log("La puntata dell'utente è di: " + userGame.toFixed())
+console.log("La puntata del computer è di: " + computerGame.toFixed())
+
+if(userGame > computerGame){
+    console.log("Congratulazioni, hai vinto!")
+}else if(computerGame > userGame){
+    console.log("Oh no, il computer ha vinto! =(")
+}else{
+    console.log("Le due puntate sono uguali, tu e il computer avete pareggiato!")
+}
